0caa7c7d31e10b6926de389e1637b296d3f0d8d4
tiwari-ashish24 <57959998+tiwari-ashish24@users.noreply.github.com>
2020-02-19 00:03:01 +0530

    Add files via upload

---
+++ Lab1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # This is Capstone Project
+# ## We will create our first project on The Battle of Neighborhood
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
